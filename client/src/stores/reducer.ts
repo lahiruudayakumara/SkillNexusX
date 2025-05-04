219231a7b1fdca5887335291343560bda82d4222
Udari Devindi <134499762+UdariDevindi@users.noreply.github.com>
2025-05-05 02:41:46 +0530

    feat: add progress reducer to combineReducers for state management

--- client/src/stores/reducer.ts
+++ client/src/stores/reducer.ts
@@ -1,8 +1,10 @@
 import authReducer from "@/stores/slices/auth/auth-slice";
 import { combineReducers } from "@reduxjs/toolkit";
+import progressReducer from "../stores/slices/progress/progress-slice";
 
 const Reducer = combineReducers({
     auth: authReducer,
+    progress: progressReducer,
 });
 
 export default Reducer;
