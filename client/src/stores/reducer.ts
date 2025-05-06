415209d4dc821dcbd77e6595623326bf1e012450
N A D NIMSARA <duvindunimsara@gmail.com>
2025-05-06 18:03:27 +0530

    feat: add learning reducer to combineReducers for state management

--- client/src/stores/reducer.ts
+++ client/src/stores/reducer.ts
@@ -1,10 +1,14 @@
 import authReducer from "@/stores/slices/auth/auth-slice";
 import { combineReducers } from "@reduxjs/toolkit";
 import progressReducer from "../stores/slices/progress/progress-slice";
+import learningReducer from "../stores/slices/learning-plan/learning-slice";
 
 const Reducer = combineReducers({
     auth: authReducer,
     progress: progressReducer,
+    learning: learningReducer,
 });
+
+
 
 export default Reducer;
